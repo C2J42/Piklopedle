750daf916b5745f01e7fc6429594eb3329533adb
c2j <cjjohnston05@gmail.com>
2023-10-25 22:08:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -18,10 +18,14 @@ function getEnemyInfo(inputName) {
     $.getJSON('creatureData.json', function(data) {
         $.each(data.creature, function(i, c) {
             if (inputName == c.name) {
-                document.getElementById("inputName").textContent = c.name
-                document.getElementById("inputWeight").textContent = c.weight
-                document.getElementById("inputFirstAppear").textContent = c.appearances.substring(1, 2)
+                document.getElementById("inputName").textContent = c.name;
+                document.getElementById("inputWeight").textContent = c.weight;
+                document.getElementById("inputFirstAppear").textContent = c.appearances.substring(1, 2);
+                return;
             }
         });
     });
+    document.getElementById("inputName").textContent = "n/a";
+    document.getElementById("inputWeight").textContent = "n/a";
+    document.getElementById("inputFirstAppear").textContent = "n/a";
 }
