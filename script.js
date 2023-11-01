febd7f907d30b9acc04db26aaf91c2f3edcb55cc
c2j <cjjohnston05@gmail.com>
2023-11-01 14:22:52 -0500

    Daily target!
    Some button layout changes as well

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 let targetCreature = null;
 /**most recent input enemy*/
 let inputCreature = null;
+/**true if target hasn't been found*/
+let gameRunning = true;
 
 /**bc copy/paste annoying*/
 let downArrow = "↓ ";
@@ -15,11 +17,21 @@ let xMark = "☒ ";
 // redo how data shown. Comparision stuff on the input fields, not target fields
 
 /**
+ * calls evaluateInput() if enter key pressed
+ */
+document.addEventListener('keydown', function(event) {
+    if (event.key == "Enter") {
+        evaluateInput();
+    }
+});
+
+/**
  * Gets info from the input field and evaluates it.
  */
 function evaluateInput() {
-    document.getElementById("output").textContent = "You said: " + document.getElementById("creatureInput").value;
-    getEnemyInfo(document.getElementById("creatureInput").value, addComparison)
+    if (gameRunning) {
+        getEnemyInfo(document.getElementById("creatureInput").value, addComparison)
+    }
 }
 
 /**
@@ -37,12 +49,20 @@ function getEnemyInfo(inputName, callback) {
                 inputCreature = c;
                 foundTarg = true;
                 callback();
+                document.getElementById("invalidInputWarning").textContent = "";
+                document.getElementById("creatureInput").value = "";
                 return;
             }
         });
         // if input enemy does not exist
         if (!foundTarg) {
-            return
+            let temp = document.getElementById("creatureInput").value;
+            if (temp.trim() != "") {
+                document.getElementById("invalidInputWarning").textContent = 
+                    "\"" + temp + "\" is not a valid input";
+            }
+            document.getElementById("creatureInput").value = "";
+            return;
         }
     });
 }
@@ -52,20 +72,55 @@ function getEnemyInfo(inputName, callback) {
  */
 function newTarget() {
     $.getJSON('creatureData.json', function(data) {
-        var keys = Object.keys(data.creature)
-        var randKey = keys[Math.floor(Math.random() * keys.length)]
-        targetCreature = data.creature[randKey]
-        console.log(targetCreature)
+        var keys = Object.keys(data.creature);
+        var randKey = keys[Math.floor(Math.random() * keys.length)];
+        targetCreature = data.creature[randKey];
+        console.log(targetCreature);
     });
     //delete all but first row
     let temp = document.getElementById("comparisonTable").rows.length;
     for (let i = 1; i < temp; i++) {
-        document.getElementById("comparisonTable").deleteRow(1)
+        document.getElementById("comparisonTable").deleteRow(1);
     }
+    gameRunning = true;
+    document.getElementById("creatureInput").style.display = "inline";
+    document.getElementById("enterButton").style.display = "inline";
+    document.getElementById("freePlayButton").style.display = "none";
 }
 
 /**
- * Compares input values and target values
+ * Gets a random target enemy, using current day as a seed
+ */
+function dailyTarget() {
+    // get day, make all days within 148 of each other equal (divison and Math.floor prob)
+    let numKeys = 148;
+    let today = new Date();
+    let acDate = new Date("10/26/2001"); // arbitrary count date - release date of Pikmin 1
+    let dayDiff = Math.floor((today.getTime()-acDate.getTime()) / (1000*3600*24));
+    let index = Math.floor(dayDiff/numKeys);
+    let seed = dayDiff % numKeys; // used as seed and target index
+    // randomize list of numbers between [0, 148) using altered day as seed
+    let numArr = [];
+    for (let i = 0; i < numKeys; i++) {
+        numArr[i] = i;
+    }
+    //   this ensures that all creatures are looped through before repitions occur
+    numArr = shuffle(numArr, seed);
+
+    // target is creatureData[dayDiff]
+    $.getJSON('creatureData.json', function(data) {
+        var keys = Object.keys(data.creature);
+        var randKey = keys[numArr[index]];
+        targetCreature = data.creature[randKey];
+        console.log(targetCreature);
+    });
+
+    document.getElementById("freePlayButton").style.display = "none";
+    // check that the day thing working correctly for periods of 148 days
+}
+
+/**
+ * Compares input values and target values. Defunct.
  */
 function compareVals() {
     if (inputCreature != null) {
@@ -148,8 +203,52 @@ function addComparison() {
         // oatchi is default because he's the best boy
         row.insertCell(5).appendChild(img);
     }
+    if (inputCreature.name == targetCreature.name) {
+        doWin();
+    }
 }
 
-function compareHelper() {
-    
+/**
+ * Things to happen when target matches input
+ */
+function doWin() {
+    gameRunning = false;
+    document.getElementById("creatureInput").style.display = "none"
+    document.getElementById("enterButton").style.display = "none"
+    document.getElementById("freePlayButton").style.display = "inline"
+}
+
+/**
+ * Array shuffler.
+ * From https://stackoverflow.com/questions/16801687/javascript-random-ordering-with-seed
+ * @param {*} array array to shuffle.
+ * @returns shuffled array
+ */
+function shuffle(array, seed) {
+    var m = array.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+        // Pick a remaining element…
+        i = Math.floor(random(seed) * m--);
+
+        // And swap it with the current element.
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+        ++seed
+    }
+    return array;
+}
+
+/**
+ * "Random" number generator to help with shuffler
+ * From https://stackoverflow.com/questions/16801687/javascript-random-ordering-with-seed
+ * @param {*} seed
+ * @returns "random" number
+ */
+function random(seed) {
+    var x = Math.sin(seed++) * 10000; 
+    return x - Math.floor(x);
 }
